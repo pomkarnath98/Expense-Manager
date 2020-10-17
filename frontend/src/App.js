b37cfbb4b544704ff119271d03ccd0f7488ab303
Sheelu27 <sheelukumari27@gmail.com>
2020-10-17 07:18:53 +0530

    Updated

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,16 @@
-import React from 'react';
-import './App.css';
-import Login from "./Components/Login"
-import { Route } from "react-router-dom"
-import Register from './Components/Register';
-
-
+import React from "react";
+import "./App.css";
+import Login from "./Components/Login";
+import { Route } from "react-router-dom";
+import Register from "./Components/Register";
+import Dashboard from "./Components/Dashboard";
 
 function App() {
   return (
     <div className="App">
-      
-      <Route path="/" exact component = {Login}/>
-      <Route path="/signup" render = {(props)=> <Register {...props}/>}/>
-     
+      {/* <Route path="/" exact component={Login} />
+      <Route path="/signup" render={(props) => <Register {...props} />} /> */}
+      <Dashboard />
     </div>
   );
 }
